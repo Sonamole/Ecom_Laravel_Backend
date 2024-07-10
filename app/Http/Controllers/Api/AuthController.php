1cb12df686fbb681a8c14708ff532ebb38141e18
Sonamole <sonamoleeldho@gmail.com>
2024-07-10 10:27:02 +0530

    Category CRUD completed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,16 +13,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    public function show()
-    {
-       $users= User::all();
-        return response()->json([
-            'status'=>200,
-             'users'=>$users,
-            'message'=>'All data displayed successfully'
-        ]);
-    }
-
+    
     public function register(Request $request)
     {
         $validator=Validator::make($request->all(),[
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::middleware('auth:sanctum','isAPIAdmin')->group(function(){//ensure that only authenticated users can access it.This groups routes that require the auth:sanctum middleware. It means any route within this group will require the user to be authenticated using Laravel Sanctum.
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('/register',[AuthController::class,'register']); //kernel.php,cors.php,User.php,AuthController
+Route::post('/login',[AuthController::class,'login']);
+
+
+Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function(){//ensure that only authenticated users can access it.This groups routes that require the auth:sanctum middleware. It means any route within this group will require the user to be authenticated using Laravel Sanctum.
     Route::get('/checkingAuthenticated',function(){
         return response()->json(['message'=> 'You are in','status'=>200],200);
     });
+
+    //category section
+    Route::get('/view-category',[CategoryController::class,'index']);
+    Route::post('/store-category',[CategoryController::class,'store']);
+    Route::get('/edit-category/{id}',[CategoryController::class,'edit']);
+    Route::put('/update-category/{id}',[CategoryController::class,'update']);
+    Route::delete('/delete-category/{id}',[CategoryController::class,'destroy']);
 });
 
 
@@ -26,14 +43,9 @@ Route::middleware('auth:sanctum')->group(function(){
 });
 
 
-Route::get('/showuser',[AuthController::class,'show']);// This route shows user information// Accessible without authentication
-Route::post('/register',[AuthController::class,'register']); //kernel.php,cors.php,User.php,AuthController
-Route::post('/login',[AuthController::class,'login']);
 
 
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
